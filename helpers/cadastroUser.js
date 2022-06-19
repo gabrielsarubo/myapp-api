5a6c827b409bd8930719b5dac1b09f1a576f28de
giovanniromanonetoUTFPR <giovannineto@alunos.utfpr.edu.br>
2022-06-19 20:11:13 -0300

    Verificacao dos campos no cadastro de usuario

--- helpers/cadastroUser.js
+++ helpers/cadastroUser.js
@@ -3,7 +3,18 @@ const NovoUsuario = require('../models/NovaUsuario')
 module.exports = {
     cadastro: async function(req, res) {
         let {admin, nome, email, senha} = req.body
-
+        if (!admin) {
+            return res.status(400).send("Insira a flag admin");
+        }
+        if (!nome) {
+            return res.status(400).send("Insira o nome");
+        }
+        if (!email) {
+            return res.status(400).send("Insira o email");
+        }
+        if (!senha) {
+            return res.status(400).send("Insira a senha");
+        }
         let confirm = await NovoUsuario.notExistEmail(email);
         let registerSucess = await NovoUsuario.register(admin, nome, email, senha);
 
