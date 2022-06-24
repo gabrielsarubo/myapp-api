93d38394ec91311eb9defaec933470d520751080
Gabriel <gabrielsarubo@hotmail.com>
2022-06-24 18:46:40 -0300

    prevent user from generating of translation exercises

--- middlewares/geradorDeQuestoes/traducao.js
+++ middlewares/geradorDeQuestoes/traducao.js
@@ -1,30 +1,46 @@
 const firebase = require('../../config/firebase')
 const bd = firebase.default.firestore();
 
-const traducao = function (req, res, next) {
-    bd.collection(`${req.body.colecao}`).where('dificuldade', '==', `${req.body.dificuldade}`)
-        .get()
-        .then((docs) => {
-            const perguntas = [];
-            docs.forEach((doc) => { perguntas.push({data: doc.data(), id: doc.id}) });
+const traducao = async (req, res, next) => {
+    const { colecao, dificuldade } = req.body
 
-            const id = Math.floor(Math.random() * docs.size);
-            const pergunta = perguntas[id]
+    try {
+        const colRef = bd.collection(colecao)
+        const snapshot = await colRef.where('dificuldade', '==', dificuldade).get()
 
-            const alt = perguntas;
-            const respostas = alt.filter(item => item.data.resposta.includes(pergunta.data.resposta) !== true);
-            const opcoes = gerarNumeros(docs.size-1);
-            const final = {
-                id: pergunta.id,
-                palavra: pergunta.data.palavra,
-                resposta: pergunta.data.resposta,
-                alternativas: [respostas[opcoes[0]].data.resposta, respostas[opcoes[1]].data.resposta, respostas[opcoes[2]].data.resposta]
-            }
-            res.json(final);
-        }).catch((error) => {
-            console.log(error);
-            res.status(400).send('Erro na geracao de pergunta');
+        const perguntas = [];
+        snapshot.forEach(doc => {
+            perguntas.push({
+                // ...doc.data(),
+                data: doc.data(),
+                id: doc.id
+            })
         })
+
+        console.log(perguntas)
+
+        // ERRO AO EXECUTAR O CODIGO ABAIXO
+        // const id = Math.floor(Math.random() * snapshot.size);
+        // const pergunta = perguntas[id]
+
+        // // alternativas == copia das perguntas
+        // const alt = [...perguntas]
+
+        // const respostas = alt.filter(item => item.data.resposta.includes(pergunta.data.resposta) !== true)
+        // const opcoes = gerarNumeros(snapshot.size - 1)
+        // const final = {
+        //     id: pergunta.id,
+        //     palavra: pergunta.data.palavra,
+        //     resposta: pergunta.data.resposta,
+        //     alternativas: [respostas[opcoes[0]].data.resposta, respostas[opcoes[1]].data.resposta, respostas[opcoes[2]].data.resposta]
+        // }
+
+        // res.json(final)
+        res.end()
+    } catch (error) {
+        console.log('Error fetching colletion in Firebase: ', error)
+        res.end()
+    }
 }
 
 const gerarNumeros = function (max) {
